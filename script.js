1bf6b2140567f7744ccf20df1196c9b727c78704
Aniket Bhogawar <39596152+1aniket@users.noreply.github.com>
2024-12-17 14:10:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const makeChange = (c) => {
   // your name here
+
+	let q=0;
+	let d=0;
+	let n=0;
+	let p=0;
+
+	q=parseInt(c/25);
+	c=c%25;
+	d=parseInt(c/10);
+	c=c%10;
+	n=parseInt(c/5);
+	c=c%5;
+
+	p=c;
+
+  
+	return {"q":q , "d":d  , "n":n , "p":p};
+	
+	
+	
 };
 
 // Do not the change the code below
